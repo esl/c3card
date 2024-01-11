6d2b743d0483fb44a8499020d43c75c6f816ba94
Ricardo Lanziano <ricardo.lanziano@erlang-solutions.com>
2024-01-10 21:03:58 -0500

    Further split configuration into different files
    Signed-off-by: Ricardo Lanziano <ricardo.lanziano@erlang-solutions.com>

--- include/config.hrl
+++ include/config.hrl
@@ -5,20 +5,8 @@
 -define(DEFAULT_NEOPIXEL_PIN, 4).
 -define(DEFAULT_NEOPIXEL_TOTAL_PIXELS, 3).
 
--ifdef(PROD). %% Production
--define(DEFAULT_STA_SSID, <<"ssid">>).
--define(DEFAULT_STA_PSK, <<"12345678">>).
--define(DEFAULT_GW_HOST, {192, 168, 0, 20}).
--define(DEFAULT_GW_DATA_PORT, 9999).
--define(DEFAULT_GW_COMM_PORT, 9998).
--define(DEFAULT_GW_HANDLER, c3card_gateway_command).
--define(DEFAULT_SENSORS, [{aht20, start_link, []}]).
--else. %% Development
--define(DEFAULT_STA_SSID, <<"ssid">>).
--define(DEFAULT_STA_PSK, <<"12345678">>).
--define(DEFAULT_GW_HOST, {192, 168, 0, 20}).
--define(DEFAULT_GW_DATA_PORT, 9999).
--define(DEFAULT_GW_COMM_PORT, 9998).
--define(DEFAULT_GW_HANDLER, c3card_gateway_command).
--define(DEFAULT_SENSORS, [{aht20, start_link, []}, {bme280, start, [{address, 16#77}]}]).
+-ifdef(PROD).
+-include("config.prod.hrl").
+-else.
+-include("config.dev.hrl").
 -endif.
